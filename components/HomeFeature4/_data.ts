bf23b6eadf53bba04d4f14dc9f475657f9f2bb73
juzhiyuan <juzhiyuan@apache.org>
2021-03-15 18:33:28 +0800

    style: update styles

--- components/HomeFeature4/_data.ts
+++ components/HomeFeature4/_data.ts
@@ -8,7 +8,7 @@ export const data: BlogData[] = [
     description:
       'Amazon Elastic Kubernetes Service（Amazon EKS）作为一种托管的 Kubernetes 服务，您可以在亚马逊云科技上轻松运行 Kubernetes 负载而无需对控制平面或节点进行安装和维护。',
     type: '博客',
-    tags: ['k8s', 'Ingress Controller', 'Amazon'],
+    tags: ['k8s', 'Amazon'],
     href: '/zh/blog/how-Amazon-EKS-and-APISIX-ingress-controller-work-together-to-manage-complex-traffic'
   }, {
     image:
@@ -27,7 +27,7 @@ export const data: BlogData[] = [
     description:
       '云原生已经成为全球企业数字化转型关键的驱动力，支流科技与吉利合作，实现对 API 的集中管理、可视化和安全保障，为吉利下一个「千万辆」时代打好基础。',
     type: '案例',
-    tags: [],
+    tags: ['吉利集团'],
     href: '/zh/usercase/geely'
   },
 ]
