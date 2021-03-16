283cea68da0c6d5092c40d4086541dfeed71728d
juzhiyuan <juzhiyuan@apache.org>
2021-03-16 17:35:00 +0800

    added title for team page

--- views/Team/index.tsx
+++ views/Team/index.tsx
@@ -1,41 +1,45 @@
 import { Box, Heading, SimpleGrid, Text } from '@chakra-ui/react'
+import { NextSeo } from 'next-seo'
 import * as React from 'react'
 import { Member } from './Member'
 import { members } from './_data'
 
 export const App = () => (
-  <Box as="section">
-    <Box
-      mx="auto"
-      maxW={{ base: 'xl', md: '7xl' }}
-      px={{ base: '6', md: '8' }}
-      py={{ base: '12', md: '20' }}
-    >
-      <Box textAlign="center">
-        <Heading size="3xl" letterSpacing="tight" mb="5" fontWeight="extrabold">
-          团队成员
+  <>
+    <NextSeo title="团队成员" />
+    <Box as="section">
+      <Box
+        mx="auto"
+        maxW={{ base: 'xl', md: '7xl' }}
+        px={{ base: '6', md: '8' }}
+        py={{ base: '12', md: '20' }}
+      >
+        <Box textAlign="center">
+          <Heading size="3xl" letterSpacing="tight" mb="5" fontWeight="extrabold">
+            团队成员
         </Heading>
-        <Text fontSize="xl" maxW="2xl" mx="auto">
-          支流科技成立于2019年，团队成员热爱开源、技术背景深厚。
+          <Text fontSize="xl" maxW="2xl" mx="auto">
+            支流科技成立于2019年，团队成员热爱开源、技术背景深厚。
         </Text>
+        </Box>
+        <SimpleGrid mt="20" columns={{ base: 1, md: 2, lg: 3 }} spacingX="6" spacingY="16">
+          {members.map((member, idx) => (
+            <Member
+              key={idx}
+              role={member.role}
+              image={member.image}
+              name={member.name}
+              twitter={member.twitter}
+              linkedIn={member.linkedin}
+              github={member.github}
+            >
+              {member.description}
+            </Member>
+          ))}
+        </SimpleGrid>
       </Box>
-      <SimpleGrid mt="20" columns={{ base: 1, md: 2, lg: 3 }} spacingX="6" spacingY="16">
-        {members.map((member, idx) => (
-          <Member
-            key={idx}
-            role={member.role}
-            image={member.image}
-            name={member.name}
-            twitter={member.twitter}
-            linkedIn={member.linkedin}
-            github={member.github}
-          >
-            {member.description}
-          </Member>
-        ))}
-      </SimpleGrid>
     </Box>
-  </Box>
+  </>
 )
 
 export default App
