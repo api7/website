248e823c6ee026e67db3b198980b0342852cd8c9
litesun <7sunmiao@gmail.com>
2021-04-04 17:57:31 +0800

    feat: update iframe height

--- views/Contributor/Contributor.tsx
+++ views/Contributor/Contributor.tsx
@@ -82,7 +82,7 @@ const Contributor: NextPage<Props, any> = ({ t, url }) => {
       </Fab>
       <NextSeo title={t(`common:contributor-graph`)} />
       <div className="iframeBox">
-        <iframe src={`https://contributor-graph.apiseven.com/?chart=${chart}&repo=${repo}`} scrolling="no" style={{ overflow: "hidden" }}></iframe>
+        <iframe src={`https://contributor-graph.apiseven.com/?chart=${chart}&repo=${repo}`} scrolling="no" style={{ overflow: "hidden",height:'1000px' }}></iframe>
       </div>
     </SWrapper>
   );
