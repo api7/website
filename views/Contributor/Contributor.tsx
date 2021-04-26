4629e23d8a8c1aba4ab8e8ed5fff12327cc83a84
litesun <7sunmiao@gmail.com>
2021-04-26 12:19:14 +0800

    feat: update merge view

--- views/Contributor/Contributor.tsx
+++ views/Contributor/Contributor.tsx
@@ -30,6 +30,7 @@ const getParameterByName = (name, url = '') => {
 const Contributor: NextPage<Props, any> = ({ t, url }) => {
   const repo = getParameterByName('repo', url) || 'apache/apisix';
   const chart = getParameterByName('chart', url);
+  const merge = getParameterByName('merge', url) || '';
   const [legend, setLegend] = useState([]);
   const [chartType, setChartType] = useState('');
   const [shareUrl, setShareUrl] = useState('https://www.apiseven.com/en/contributor-graph')
@@ -83,7 +84,7 @@ const Contributor: NextPage<Props, any> = ({ t, url }) => {
       </Fab>
       <NextSeo title={t(`common:contributor-graph`)} />
       <div className="iframeBox">
-        <iframe src={`https://contributor-graph.apiseven.com/?chart=${chart}&repo=${repo}`} scrolling="no" style={{ overflow: "hidden", height: chartType === 'contributorMonthlyActivity' ? '1000px' : '1200px' }}></iframe>
+        <iframe src={`https://contributor-graph.apiseven.com/?chart=${chart}&repo=${repo}${merge ? '&merge=true' : ''}`} scrolling="no" style={{ overflow: "hidden", height: chartType === 'contributorMonthlyActivity' ? '1000px' : '1200px' }}></iframe>
       </div>
     </SWrapper>
   );
