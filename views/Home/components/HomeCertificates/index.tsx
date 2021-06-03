76faf523e1a4a6b99170921bb7894b4b0cb7134e
juzhiyuan <juzhiyuan@apache.org>
2021-06-03 17:31:18 +0800

    feat: added cert

---
+++ views/Home/components/HomeCertificates/index.tsx
@@ -0,0 +1,62 @@
+import {
+  Heading, Box, Stack, Text, useColorModeValue, StackDivider, Image
+} from '@chakra-ui/react'
+import * as React from 'react'
+import { NextPage } from 'next'
+import { TFunction, I18nContext } from "next-i18next";
+
+import { withTranslation } from '../../../../i18n'
+
+type Props = {
+  t: TFunction;
+};
+
+const App: NextPage<Props, any> = ({ t }) => {
+  const {
+    i18n: {
+      language
+    }
+  } = React.useContext(I18nContext)
+
+  return (
+    <>
+      <Box as="section" maxW="7xl" mx="auto" px={{ base: '6', md: '8' }} py={{ base: '12', md: '20' }}>
+        <Box mb="12" textAlign="center">
+          <Heading size="2xl" fontWeight="extrabold" lineHeight="normal">
+            可靠的高性能网关
+          </Heading>
+          <Text
+            fontSize="lg"
+            mt="4"
+            fontWeight="medium"
+            color={useColorModeValue('gray.600', 'whiteAlpha.700')}
+          >
+            获得厂商和权威机构认证
+          </Text>
+        </Box>
+        {language === 'zh-CN' && (
+          <Stack spacing="8" direction={{ base: 'column', md: 'row' }} justifyContent="space-around">
+            <Box textAlign="center">
+              <Image src="https://static.apiseven.com/logo-huawei-compatible.png" maxH="130px" />
+              <Text>华为云 Stack 8.0（鲲鹏）</Text>
+            </Box>
+            <Box textAlign="center">
+              <Image src="https://static.apiseven.com/logo-huawei-compatible.png" maxH="130px" />
+              <Text>华为云鲲鹏云服务</Text>
+            </Box>
+            <Box textAlign="center">
+              <Image src="https://static.apiseven.com/%E5%8F%AF%E4%BF%A1%E4%BA%91.jpeg" maxH="130px" margin="0 auto" />
+              <Text>可信开源项目</Text>
+            </Box>
+          </Stack>
+        )}
+      </Box>
+    </>
+  )
+}
+
+App.getInitialProps = async () => ({
+  namespacesRequired: ["common", "home"],
+});
+
+export default withTranslation("home")(App)
